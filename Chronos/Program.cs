05167a617228e2f996cf9b1eab275261ec24a263
João Borges <joaonunos.borges@gmail.com>
2022-03-27 02:31:47 +0100

    Exercicio 4 Start cron1, cron2 and ThreadSleep

---
+++ Chronos/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+
+namespace Chronos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stopwatch cron1 = new Stopwatch();
+            Stopwatch cron2 = new Stopwatch();
+
+            cron1.Start();
+            Thread.Sleep(1000);
+            cron2.Start();
+        }
+    }
+}
